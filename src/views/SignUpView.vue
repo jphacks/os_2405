b1d0eb7c7150ae711abe01f18d025fc48d8cbb1f
YumaHozumi <cpucmptrpython72@gmail.com>
2024-10-13 22:39:46 +0900

    feat: メアド〜パスワード入力部分まで実装

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue';
+import InputMail from '@/components/InputMail.vue';
+import InputPassword from '@/components/InputPassword.vue';
+
+const password = ref('');
+const passwordConfirmation = ref('');
+const email = ref('');
+</script>
+
+<template>
+    <div class="my-form">
+        <h1 class="title">新規登録</h1>
+        <InputMail 
+            label="メールアドレス"
+            placeholder="sample@example.com" 
+            v-model:email="email"
+        />
+        <InputPassword 
+            label="パスワード"
+            placeholder="半角英数字記号のみ" 
+            v-model:password="password"
+        /> 
+        <InputPassword 
+            label="パスワード（再入力）"
+            placeholder="半角英数字記号のみ"
+            v-model:password="passwordConfirmation"
+        />
+        <v-btn
+            color="primary"
+            class="my-btn"
+        >登録</v-btn>
+    </div>
+</template>
+
+<style scoped>
+.title {
+    color: #1A69C8;
+    text-align: center;
+}
+
+.center-block {
+    margin: 20px auto;
+}
+
+.my-form {
+    width: 80%;
+    margin: 0 auto;
+}
+
+.my-btn {
+    width: 100%;
+    margin-top: 20px;
+}
+</style>
