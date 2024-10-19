16b81541059fbc396417a033e48a3ba96db2b501
YumaHozumi <cpucmptrpython72@gmail.com>
2024-10-19 16:05:16 +0900

    feat: メアド確認の完了ページ&プロフィール登録完了のページを作成

---
+++ src/views/MailVerificationCompletedView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import VerifyCompletedView from '@/views/VerifyCompletedView.vue';
+</script>
+
+<template>
+    <VerifyCompletedView 
+        message="メールアドレスの認証が完了しました。"
+        buttonText="プロフィールの登録"
+    />
+</template>
---
+++ src/views/ProfileRegisterCompletedView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import VerifyCompletedView from '@/views/VerifyCompletedView.vue';
+</script>
+
+<template>
+    <VerifyCompletedView 
+        message="プロフィールの登録が完了しました。"
+        buttonText="ログインする"
+    />
+</template>
