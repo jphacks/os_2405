2f668f1f8a6f22347aaca8c5a688b42d5308d997
YumaHozumi <cpucmptrpython72@gmail.com>
2024-10-25 14:59:32 +0900

    feat: ホーム画面右下に表示するボタン群を作成

---
+++ src/components/FloatingActionButtons.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+    onClickPlusButton: {
+        type: Function,
+        default: () => { console.log('TODO: プラスボタンが押されました') }
+    },
+    onClickMenuButton: {
+        type: Function,
+        default: () => { console.log('TODO: メニューボタンが押されました') }
+    }
+})
+</script>
+
+<style scoped>
+.floating-action-wrapper {
+  position: fixed;
+  bottom: 24px;
+  right: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+</style>
+
+<template>
+  <div class="floating-action-wrapper">
+    <div class="floating-action-buttons">
+        <v-btn
+            fab
+            color="primary"
+            icon="mdi-pen-plus"
+            class="me-4"
+            @click="onClickPlusButton"
+        >
+        </v-btn>
+        <v-btn
+            fab
+            color="primary"
+            icon="mdi-menu"
+            @click="onClickMenuButton"
+        >
+        </v-btn>
+    </div>
+    
+  </div>
+</template>
