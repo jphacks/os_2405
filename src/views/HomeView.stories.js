6f8b6dabae2228f2bb743cb9c56ad177c9dcbbff
YumaHozumi <cpucmptrpython72@gmail.com>
2024-10-25 14:23:23 +0900

    feat: タスク一覧を表示するところ作成

--- src/views/HomeView.stories.js
+++ src/views/HomeView.stories.js
@@ -1,11 +1,23 @@
+// stories/TodoList.stories.js
 import HomeView from '@/views/HomeView.vue';
 
 export default {
   title: 'HomeView',
   component: HomeView,
-};
+}
 
-export const Default = () => ({
+const Template = (args) => ({
   components: { HomeView },
-  template: '<HomeView />',
-});
+  setup() {
+    return { args }
+  },
+  template: '<HomeView v-bind="args" />',
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  items: [
+    { id: 1, title: 'Task 1', datetime: '2023-05-01', description: 'Description 1', quantity: 1 },
+    { id: 2, title: 'Task 2', datetime: '2023-05-02', description: 'Description 2', quantity: 2 },
+  ],
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AddItemDialog from '@/components/AddItemDialog.vue';
+import TodoItem from '@/components/TodoItem.vue';
 import { ref } from 'vue';
 
 const dialog = ref(false);
@@ -7,9 +8,32 @@ const dialog = ref(false);
 const openDialog = () => {
     dialog.value = true;
 }
+
+defineProps({
+    items: {
+        type: Array,
+        default: () => []
+    }
+})
+
 </script>
 
 <template>
+    <v-sheet color="red-lighten-1">
+        あなたのタスク一覧
+    </v-sheet>
+
+    <TodoItem 
+        v-for="item in items"
+        :key="item"
+        :id="item.id"
+        :title="item.title"
+        :datetime="item.datetime"
+        :description="item.description"
+        :quantity="item.quantity"
+        class="my-8"
+    />
+
     <v-dialog
         v-model="dialog"
         max-width="400"
@@ -20,3 +44,14 @@ const openDialog = () => {
 
     <v-btn @click="openDialog">Open Dialog</v-btn>
 </template>
+
+<style scoped>
+.v-sheet {
+    display: inline-block;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    font-weight: bold;
+    font-size: larger;
+}
+</style>
