926c02249d63faf297a16fbac5edd475de459b40
YumaHozumi <cpucmptrpython72@gmail.com>
2024-10-26 15:43:59 +0900

    feat: メモ一覧のview作成

---
+++ src/views/MemoView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import MemoContainer from './containers/MemoContainer.vue';
+
+//TODO: 後で変える
+const sampleMemos = [
+    {
+        id: 1,
+        title: 'サンプルメモ1',
+        deadline: '2021-12-31',
+        quantity: 1,
+    }
+]
+</script>
+
+<template>
+    <MemoContainer
+        title="後で変える"
+        :items="sampleMemos"
+    ></MemoContainer>
+
+</template>
