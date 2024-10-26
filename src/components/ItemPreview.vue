5a4251b6dda8a50b8de529b41f3a6e0e794d0577
HayatoHirakawa <h.hirakawa1216@gmail.com>
2024-10-26 13:58:17 +0900

    feat: アイテムプレビューの「編集」ボタン → 商品追加ウィンドウを呼び出せるように

--- src/components/ItemPreview.vue
+++ src/components/ItemPreview.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ItemDataField from '@/components/ItemDataField.vue';
+import AddItemDialog from '@/components/AddItemDialog.vue';
 import { ref } from 'vue';
 
 defineProps({
@@ -10,52 +11,64 @@ defineProps({
     datafield_width: {
         type: String,
         default: '80%'
-    }
+    },
 });
 
-const dialog = ref(true);
+const dialog = ref(false);
+
+const openDialog = () => {
+    dialog.value = true;
+}
+
+const onClickEditButton = () => {
+    openDialog();
+}
+
 </script>
 
 <template>
-    <v-dialog v-model="dialog" max-width="800">
-        <v-card class="py-10">
-            <v-row justify="center">
-                <v-col cols="auto">
-                    <h1>
-                        商品名
-                    </h1>
-                </v-col>
-            </v-row>
+    <v-card class="py-10">
+        <v-row justify="center">
+            <v-col cols="auto">
+                <h1>
+                    商品名
+                </h1>
+            </v-col>
+        </v-row>
 
-            <v-row justify="center" class="my-4">
-                <v-col cols="auto">
-                    <v-btn
-                        color="primary"
-                        class="mybtn font-weight-bold"
-                        @click="dialog = false"
-                    >
-                        編集
-                    </v-btn>
-                </v-col>
-            </v-row>
+        <v-row justify="center" class="my-4">
+            <v-col cols="auto">
+                <v-btn
+                    color="primary"
+                    class="mybtn font-weight-bold"
+                    @click="onClickEditButton"
+                >
+                    編集
+                </v-btn>
+            </v-col>
+        </v-row>
+            
+        <!-- ItemDataFieldを動的に生成 -->
+        <v-row justify="center">
+            <v-col v-for="(info, index) in informations" :key="index" cols="12" class=" text-center">
+                <v-divider></v-divider>
 
-                
-               
-            <!-- ItemDataFieldを動的に生成 -->
-            <v-row justify="center">
-                <v-col v-for="(info, index) in informations" :key="index" cols="12" class=" text-center">
-                    <v-divider></v-divider>
+                <ItemDataField
+                    :label="info.label"
+                    :value="info.value"
+                    :width="datafield_width"
+                    class="my-4"
+                ></ItemDataField>
+            </v-col>
+        </v-row>
+    </v-card>
+    
 
-                    <ItemDataField
-                        :label="info.label"
-                        :value="info.value"
-                        :width="datafield_width"
-                        class="my-4"
-                    ></ItemDataField>
-                </v-col>
-            </v-row>
-        </v-card>
-        
+    <v-dialog
+        v-model="dialog"
+        max-width="400"
+    >
+        <AddItemDialog />
     </v-dialog>
 </template>
 
